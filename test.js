38f048b9eb1209da8b4fd6c5c396836e29c49603
arjunq21 <arjunadhikari789@gmail.com>
2023-05-05 11:46:38 +0545

    Made basic server listening on port 3000

--- test.js
+++ test.js
@@ -1,4 +1,13 @@
 // importing express
-const express = require("express")
+const express = require("express");
 
+const httpServer = express();
 
+httpServer.get("/", function(req, res){
+    console.log("New request found") ;
+    res.send("Hello There")
+})
+
+httpServer.listen(3000, function () {
+  console.log("Server listening on port 3000");
+});
