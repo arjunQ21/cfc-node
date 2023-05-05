1a3db7568b9cad3889a6bd8f5cc9128ee39c3d1c
arjunq21 <arjunadhikari789@gmail.com>
2023-05-05 12:45:01 +0545

    renamed

--- routes.js
+++ routes.js
@@ -1,7 +1,5 @@
 const express = require("express")
-
 const router = express.Router() ;
-
 router.get("/", function(req, res){
     console.log("New request found") ;
     res.send("Hello There from here")
@@ -10,5 +8,4 @@ router.post("/", function(req, res){
     console.log("New POST request") ;
     res.send("Hello from post request")
 })
-
 module.exports = router ;
--- test.js
+++ test.js
@@ -1,10 +1,10 @@
 // importing express
 const express = require("express");
-const routes = require("./routes")
+const routesFromAnotherFile = require("./routes")
 
 const httpServer = express();
 
-httpServer.use(routes)
+httpServer.use(routesFromAnotherFile)
 
 httpServer.listen(3000, function () {
   console.log("Server listening on port 3000");
