129e9b1a07857c230cdade15b58ae0cc4cd155a1
arjunq21 <arjunadhikari789@gmail.com>
2023-05-05 13:01:21 +0545

    Sending Data from query string

--- routes.js
+++ routes.js
@@ -1,11 +1,14 @@
 const express = require("express")
 const router = express.Router() ;
-router.get("/", function(req, res){
-    console.log("New request found") ;
-    res.send("Hello There from here")
+
+router.get("/hi", function(req, res){
+    console.log(req.query)
+    res.send("Hello, " + req.query.name) ;
 })
-router.post("/", function(req, res){
-    console.log("New POST request") ;
-    res.send("Hello from post request")
+
+router.get("/bye", function(req, res){
+    res.send("Bye Bye")
 })
+
+
 module.exports = router ;
