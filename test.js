806867e822ee11fb87e1ea899c007df9e49f335b
arjunq21 <arjunadhikari789@gmail.com>
2023-05-05 12:37:01 +0545

    Added POST Request

--- test.js
+++ test.js
@@ -5,7 +5,11 @@ const httpServer = express();
 
 httpServer.get("/", function(req, res){
     console.log("New request found") ;
-    res.send("Hello There")
+    res.send("Hello There from here")
+})
+httpServer.post("/", function(req, res){
+    console.log("New POST request") ;
+    res.send("Hello from post request")
 })
 
 httpServer.listen(3000, function () {
