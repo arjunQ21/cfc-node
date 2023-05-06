a0ea44d220bb4ce5d6865ba57bf570bc82f83bc7
arjunq21 <arjunadhikari789@gmail.com>
2023-05-06 12:07:26 +0545

    Getting all posts done

--- routes.js
+++ routes.js
@@ -1,16 +1,28 @@
 const express = require("express");
 const router = express.Router();
 const createNewPost = require("./functions");
+const Post = require("./db/posts");
 
+// creating new Post
 router.post("/", function (req, res) {
-  // { postContent: 'Post from API', postCreatedBy: 'THunder Client' }
-
   createNewPost(req.body.postContent, req.body.postCreatedBy)
     .then(function (post) {
-      res.send("Post Created successfully");
+      res.send(post);
+      // res.send("Post Created successfully");
     })
     .catch(function (error) {
       res.send("Error Creating Post");
+    });
+});
+
+// getting all posts from database
+router.get("/", function (req, res) {
+  Post.find()
+    .then(function (posts) {
+      res.send(posts);
+    })
+    .catch(function (error) {
+      res.send("Error getting posts: " + error);
     });
 });
 
